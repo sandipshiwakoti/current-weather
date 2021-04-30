3088f7cde76f225ae8f4924a7369ad7e3525a48b
Sandip Shiwakoti <62270918+sandipshiwakoti@users.noreply.github.com>
2021-04-30 21:10:48 +0545

    remove continous integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { useFetch } from "./useFetch";
 import Home from "./components/Home";
 import Weather from "./components/Weather";
 
